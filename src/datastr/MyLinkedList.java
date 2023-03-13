6bbea121a53b72db171df0d18d1d8998f8307d21
NiklavsBergs <bergs.niklavs@gmail.com>
2023-03-13 10:39:17 +0200

    add function

--- src/datastr/MyLinkedList.java
+++ src/datastr/MyLinkedList.java
@@ -23,14 +23,54 @@ public class MyLinkedList<T> {
 		}
 	}
 	
-	public void deleteLast() {
+	public void removeLast() {
 		last.getPrevious().setNext(null);
 		elementCount--;
 	}
 	
-	public void deleteFirst() {
+	public void removeFirst() {
 		first.getNext().setPrevious(null);
 		elementCount--;	
+	}
+	
+	public void add(T newElement, int position) throws Exception{
+		
+		if (position < 0 || position > elementCount) {
+			throw (new Exception ("Wrong position"));
+		}
+		else {
+			if (position == 0) {
+				MyNode<T> newNode = new MyNode<T>(newElement);
+				first.setPrevious(newNode);
+				newNode.setNext(first);
+				first = newNode;
+				elementCount++;
+			}
+			else if (position == elementCount) {
+				add(newElement);
+			}
+			else {
+				MyNode tempNode = first;
+				for(int i = 0; i < position-1; i++) {
+					tempNode = tempNode.getNext();
+				}
+				
+				MyNode tempNode2 = tempNode.getNext();
+				MyNode<T> newNode = new MyNode<T>(newElement);
+				
+				tempNode.setNext(newNode);
+				newNode.setPrevious(tempNode);
+				
+				tempNode2.setPrevious(newNode);
+				newNode.setNext(tempNode2);
+				
+				/*MyNode<T> newNode = new MyNode<T>(newElement);
+				newNode.setNext(tempNode.getNext());
+				newNode.setPrevious(tempNode);
+				tempNode.setNext(newNode);
+				elementCount++;*/
+			}
+		}
 	}
 	
 }
